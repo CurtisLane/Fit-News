634e40e428c3d02c1fb1ec491f9970813a9b1e6f
Curtis Lane <clanelmt@gmail.com>
2020-08-01 14:08:21 -0400

    add files/folders, html route

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
 // Dependencies
 const express = require("express");
-const mongojs = require("mongojs");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const path = require('path');
+const PORT = process.env.PORT || 8080;
+
 
 // Initialize Express
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Set up a static folder (public) for our web app
 app.use(express.static("app/public"));
+
+// require our routes
+require("./app/routes/apiRoutes.js")(app);
+require("./app/routes/htmlRoutes.js")(app);
+
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
-/*
-// Database config
-const databaseUrl;
-const collections;
 
-// MongoJs config
-var db = mongojs(databaseUrl, collections);
 
-// Error check
-db.on("error", function(error) {
-    console.log("Database Error:", error);
-});
-*/
-
-// Set the app to listen on port 8080
 app.listen(8080, function() {
     console.log("App listening on port 8080!");
 });
